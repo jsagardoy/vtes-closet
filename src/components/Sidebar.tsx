bf99c1845b06232d6aea84916cbbdac91dfd7913
Jehú <jehu.sagardoy@gmail.com>
2021-10-06 10:19:01 +0200

    sidebar private done

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,54 @@
 import { List, ListItem } from '@material-ui/core';
 import { ListItemText } from '@mui/material';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import React from 'react';
 
 import './Sidebar.css';
 const Sidebar = () => {
+  const auth = getAuth();
+  const [user, setUser] = React.useState<any>(auth);
+
+  onAuthStateChanged (auth, (us) => {
+    if (us) {
+      setUser(us);
+    }
+    else {
+      setUser(null);
+    }
+  });
+
   return (
     <div className='sidebar'>
       <div className='list__container'>
         <List className='list'>
+          <ListItem
+            alignItems='flex-start'
+            button
+            dense
+            divider
+            style={{ borderBottomColor: 'darkcyan' }}
+          >
+            <ListItemText primary='Crypt' />
+          </ListItem>
+          <ListItem
+            alignItems='flex-start'
+            button
+            dense
+            divider
+            style={{ borderBottomColor: 'darkcyan' }}
+          >
+            <ListItemText primary='Library' />
+          </ListItem>
+          <div className='privateSidebar' hidden={user?false:true}>
+            <ListItem
+              alignItems='flex-start'
+              button
+              dense
+              divider
+              style={{ borderBottomColor: 'darkcyan' }}
+            >
+              <ListItemText primary='Decks' />
+          </ListItem>
           <ListItem
             className='listItem'
             alignItems='flex-start'
@@ -40,33 +82,7 @@ const Sidebar = () => {
               }}
             />
           </ListItem>
-          <ListItem
-            alignItems='flex-start'
-            button
-            dense
-            divider
-            style={{ borderBottomColor: 'darkcyan' }}
-          >
-            <ListItemText primary='Crypt' />
-          </ListItem>
-          <ListItem
-            alignItems='flex-start'
-            button
-            dense
-            divider
-            style={{ borderBottomColor: 'darkcyan' }}
-          >
-            <ListItemText primary='Library' />
-          </ListItem>
-          <ListItem
-            alignItems='flex-start'
-            button
-            dense
-            divider
-            style={{ borderBottomColor: 'darkcyan' }}
-          >
-            <ListItemText primary='Decks' />
-          </ListItem>
+          </div>
         </List>
       </div>
     </div>
