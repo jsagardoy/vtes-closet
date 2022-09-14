60a486d93df190624a3510bb144100f498cc8d11
Jehú <jehu.sagardoy@gmail.com>
2022-09-14 12:47:32 +0200

    css for dialog more, fixed

--- src/components/main/components/crypt/MoreFilterModal.tsx
+++ src/components/main/components/crypt/MoreFilterModal.tsx
@@ -4,14 +4,18 @@ import {
   Button,
   Checkbox,
   Dialog,
+  DialogContent,
   Divider,
   FormControl,
+  FormControlLabel,
+  FormGroup,
   IconButton,
   InputLabel,
   MenuItem,
   Paper,
   Select,
   SelectChangeEvent,
+  Typography,
 } from '@mui/material';
 import React from 'react';
 import { discType, PropType } from '../../../../types/crypt_type';
@@ -100,248 +104,339 @@ const MoreFilterModal = (props: Props) => {
   } = props;
   return (
     <Dialog
+      sx={{ display: 'flex', justifyContent: 'center' }}
       open={showMore}
       onClose={() => handleMore()}
-      className='navbar__bottom'
     >
-      <Paper className='filter__container'>
-        <Box className='disc__container'>
-          {getDiscIcon(selected_discList.name).map((dis, index) => {
-            return (
-              <IconButton
-                key={dis}
-                size='small'
-                onClick={() => handleSelectDisc(index)}
-              >
-                {selected_discList.value[index] === 0 ? (
-                  <Avatar className='opacity__disc' src={dis} alt={dis} />
-                ) : selected_discList.value[index] === 1 ? (
-                  <Avatar src={dis} alt={dis} />
-                ) : (
-                  <Avatar
-                    src={getDiscIcon(disc_sup)[index]}
-                    alt={getDiscIcon(disc_sup)[index]}
-                  />
-                )}
-              </IconButton>
-            );
-          })}
-        </Box>
-        <Divider />
-        <Box className='filter__clan'>
-          <InputLabel>Clan</InputLabel>
-          <FormControl variant='standard'>
-            <Select
-              id='clan__select__id'
-              labelId='select__clan__standard__label'
-              className='select__clan'
-              value={selectedClan}
-              onChange={handleClan}
-              label='Clan'
-            >
-              <MenuItem value=''>
-                <em>None</em>
-              </MenuItem>
-              {getClans().map((clan) => (
-                <MenuItem key={clan} value={clan}>
-                  <Avatar
-                    className='select__clan__avatar__icon'
-                    src={getClanIcon([clan]).find((c) => c)}
-                    alt={clan}
-                  />
-                  {clan}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Box>
-        <Divider />
-
-        <Box className='filter__sect'>
-          <InputLabel>Sect</InputLabel>
-          <FormControl variant='standard'>
-            <Select
-              id='select__sect__id'
-              labelId='select__sect__standard__label'
-              className='select__sect'
-              value={selectedSect}
-              onChange={handleSect}
-              label='Sect'
-            >
-              <MenuItem value=''>
-                <em>None</em>
-              </MenuItem>
-              {getSects().map((sect) => (
-                <MenuItem key={sect} value={sect}>
-                  {sect}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Box>
-        <Divider />
-        <Box className='title__container'>
-          {getTitle().map((title: string) => (
-            <Box key={title} className='title__pair'>
-              <Checkbox
-                size='small'
-                checked={checkboxTitle(title)}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  handleTitle(title, e)
-                }
-              />
-              <InputLabel>{title}</InputLabel>
-            </Box>
-          ))}
-        </Box>
-        <Divider />
-        <Box className='filter__slider__group'>
-          <SlidersComponent
-            filterSliders={handleSliders}
-            group={selectedGroup}
-            maxCapacity={selectedMaxCap}
-            minCapacity={selectedMinCap}
-            groups={groups}
-            handleSliderGroup={handleSliderGroup}
-            handleSliderMaxCap={handleSliderMaxCap}
-            handleSliderMinCap={handleSliderMinCap}
-          />
-        </Box>
-        <Divider />
-        <Box className='filter__props'>
-          <Box className='prop__column'>
-            <Box className='prop__pair'>
-              <Checkbox
-                size='small'
-                checked={checked.bleed}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  handleCheck('bleed', e)
-                }
-              />
-              <InputLabel>+ Bleed</InputLabel>
-            </Box>
-            <Box className='prop__pair'>
-              <Checkbox
-                size='small'
-                checked={checked.strength}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  handleCheck('strength', e)
-                }
-              />
-              <InputLabel>+ Strength</InputLabel>
-            </Box>
-            <Box className='prop__pair'>
-              <Checkbox
-                size='small'
-                checked={checked.stealth}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  handleCheck('stealth', e)
-                }
-              />
-              <InputLabel>+ Stealth</InputLabel>
-            </Box>
-            <Box className='prop__pair'>
-              <Checkbox
-                size='small'
-                checked={checked.intercept}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  handleCheck('intercept', e)
-                }
-              />
-              <InputLabel>+ Intercept</InputLabel>
-            </Box>
-            <Box className='prop__pair'>
-              <Checkbox
-                size='small'
-                checked={checked.aggravated}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  handleCheck('aggravated', e)
-                }
-              />
-              <InputLabel>Aggravated</InputLabel>
-            </Box>
-
-            <Box className='prop__pair'>
-              <Checkbox
-                size='small'
-                checked={checked.enter_combat}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  handleCheck('enter_combat', e)
-                }
-              />
-              <InputLabel>Enter combat</InputLabel>
-            </Box>
+      <DialogContent>
+        <Paper
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+          }}
+        >
+          <Box id='disciplines' sx={{ m: '1rem' }}>
+            {getDiscIcon(selected_discList.name).map((dis, index) => {
+              return (
+                <IconButton
+                  key={dis}
+                  size='small'
+                  onClick={() => handleSelectDisc(index)}
+                >
+                  {selected_discList.value[index] === 0 ? (
+                    <Avatar className='opacity__disc' src={dis} alt={dis} />
+                  ) : selected_discList.value[index] === 1 ? (
+                    <Avatar src={dis} alt={dis} />
+                  ) : (
+                    <Avatar
+                      src={getDiscIcon(disc_sup)[index]}
+                      alt={getDiscIcon(disc_sup)[index]}
+                    />
+                  )}
+                </IconButton>
+              );
+            })}
           </Box>
-          <Box className='prop__column'>
-            <Box className='prop__pair'>
-              <Checkbox
-                size='small'
-                checked={checked.flight}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  handleCheck('flight', e)
-                }
-              />
-              <InputLabel>Flight</InputLabel>
-            </Box>
-            <Box className='prop__pair'>
-              <Checkbox
-                size='small'
-                checked={checked.black_hand}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  handleCheck('black_hand', e)
-                }
-              />
-              <InputLabel>Black Hand</InputLabel>
-            </Box>
-            <Box className='prop__pair'>
-              <Checkbox
-                size='small'
-                checked={checked.red_list}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  handleCheck('red_list', e)
-                }
-              />
-              <InputLabel>Red List</InputLabel>
-            </Box>
-            <Box className='prop__pair'>
-              <Checkbox
-                size='small'
-                checked={checked.infernal}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  handleCheck('infernal', e)
-                }
-              />
-              <InputLabel>Infernal</InputLabel>
-            </Box>
-            <Box className='prop__pair'>
-              <Checkbox
-                size='small'
-                checked={checked.slave}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  handleCheck('slave', e)
-                }
-              />
-              <InputLabel>Slave</InputLabel>
-            </Box>
-          </Box>
-        </Box>
-        <Divider />
-        <Box className='clear__button'>
-          <Button
+          <Divider />
+          <Box
+            id='selectors'
             sx={{
-              textTransform: 'capitalize',
-              border: '1px solid black',
-              borderRadius: '2px',
-              marginTop: '1rem',
-              marginBottom: '1rem',
+              display: 'flex',
+              alignContent: 'center',
+              justifyContent: 'space-between',
+              m: '1rem',
             }}
-            size='small'
-            onClick={() => handleResetButton()}
           >
-            Reset Filters
-          </Button>
-        </Box>
-      </Paper>
+            <Box
+              id='selectClan'
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'flex-start',
+                alignItems: 'center',
+                maxHeight: '5vh',
+                height: '5vh',
+              }}
+            >
+              <InputLabel>Clan</InputLabel>
+              <FormControl variant='standard'>
+                <Select
+                  sx={{ minWidth: '10rem', m: '1rem' }}
+                  id='clan__select__id'
+                  labelId='select__clan__standard__label'
+                  value={selectedClan}
+                  onChange={handleClan}
+                  label='Clan'
+                >
+                  <MenuItem value=''>
+                    <em>None</em>
+                  </MenuItem>
+                  {getClans().map((clan) => (
+                    <MenuItem key={clan} value={clan}>
+                      <Avatar
+                        src={getClanIcon([clan]).find((c) => c)}
+                        alt={clan}
+                      />
+                      {clan}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+            <Box
+              id='selectSect'
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'flex-start',
+                alignItems: 'center',
+                maxHeight: '5vh',
+                height: '5vh',
+              }}
+            >
+              <InputLabel>Sect</InputLabel>
+              <FormControl
+                sx={{
+                  maxHeight: '5vh',
+                  height: '5vh',
+                }}
+                variant='standard'
+              >
+                <Select
+                  sx={{ minWidth: '10rem', m: '1rem' }}
+                  id='select__sect__id'
+                  labelId='select__sect__standard__label'
+                  value={selectedSect}
+                  onChange={handleSect}
+                  label='Sect'
+                >
+                  <MenuItem value=''>
+                    <em>None</em>
+                  </MenuItem>
+                  {getSects().map((sect) => (
+                    <MenuItem key={sect} value={sect}>
+                      <Typography variant='body1'>{sect}</Typography>
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+          </Box>
+          <Divider />
+          <Box
+            sx={{
+              display: 'flex',
+              m: '1rem',
+            }}
+          >
+            <FormGroup
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'flex-start',
+                gap: '.5rem',
+                maxHeight: '19vh',
+              }}
+            >
+              {getTitle().map((title: string) => (
+                <FormControlLabel
+                  key={title}
+                  control={
+                    <Checkbox
+                      size='small'
+                      checked={checkboxTitle(title)}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        handleTitle(title, e)
+                      }
+                    />
+                  }
+                  label={title}
+                />
+              ))}
+            </FormGroup>
+          </Box>
+          <Divider />
+          <Box id='sliders'>
+            <SlidersComponent
+              filterSliders={handleSliders}
+              group={selectedGroup}
+              maxCapacity={selectedMaxCap}
+              minCapacity={selectedMinCap}
+              groups={groups}
+              handleSliderGroup={handleSliderGroup}
+              handleSliderMaxCap={handleSliderMaxCap}
+              handleSliderMinCap={handleSliderMinCap}
+            />
+          </Box>
+          <Divider />
+          <Box sx={{ display: 'flex', m: '1rem' }}>
+            <FormGroup
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'flex-start',
+                gap: '.5rem',
+                maxHeight: '15vh',
+              }}
+            >
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    size='small'
+                    checked={checked.bleed}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      handleCheck('bleed', e)
+                    }
+                  />
+                }
+                label='+ Bleed'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    size='small'
+                    checked={checked.strength}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      handleCheck('strength', e)
+                    }
+                  />
+                }
+                label='+ Strength'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    size='small'
+                    checked={checked.stealth}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      handleCheck('stealth', e)
+                    }
+                  />
+                }
+                label='+ Stealth'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    size='small'
+                    checked={checked.intercept}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      handleCheck('intercept', e)
+                    }
+                  />
+                }
+                label='+ Intercept'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    size='small'
+                    checked={checked.aggravated}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      handleCheck('aggravated', e)
+                    }
+                  />
+                }
+                label='Aggravated'
+              />
+
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    size='small'
+                    checked={checked.enter_combat}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      handleCheck('enter_combat', e)
+                    }
+                  />
+                }
+                label='Enter combat'
+              />
+
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    size='small'
+                    checked={checked.flight}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      handleCheck('flight', e)
+                    }
+                  />
+                }
+                label='Flight'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    size='small'
+                    checked={checked.black_hand}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      handleCheck('black_hand', e)
+                    }
+                  />
+                }
+                label='Black Hand'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    size='small'
+                    checked={checked.red_list}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      handleCheck('red_list', e)
+                    }
+                  />
+                }
+                label='Red List'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    size='small'
+                    checked={checked.infernal}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      handleCheck('infernal', e)
+                    }
+                  />
+                }
+                label='Infernal'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    size='small'
+                    checked={checked.slave}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      handleCheck('slave', e)
+                    }
+                  />
+                }
+                label='Slave'
+              />
+            </FormGroup>
+          </Box>
+          <Divider />
+          <Box
+            sx={{ display: 'flex', justifyContent: 'space-around', m: '1rem' }}
+          >
+            <Button
+              sx={{ border: '1px solid' }}
+              color='secondary'
+              onClick={() => handleResetButton()}
+            >
+              Reset Filters
+            </Button>
+            <Button
+              sx={{ border: '1px solid' }}
+              color='secondary'
+              onClick={() => handleMore()}
+            >
+              Done
+            </Button>
+          </Box>
+        </Paper>
+      </DialogContent>
     </Dialog>
   );
 };
